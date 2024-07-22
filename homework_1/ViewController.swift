c81c53d333833ce864bfc05caa8d1550f1d71cd4
Dmitriy Makeev <makeev095095@mail.ru>
2024-07-22 13:54:57 +0300

    homework_2

--- homework_1/ViewController.swift
+++ homework_1/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  homework_1
+//  homework_2
 //
 //  Created by Макей 😈 on 15.07.2024.
 //
@@ -9,100 +9,104 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let nameLabel: UILabel = UILabel()
-    private let nameLabelValue: UILabel = UILabel()
+    lazy var oldPasswordField: UITextField = configurePasswordField(placeholderText: "Старый пароль", yPossition: 580)
+    lazy var newPasswordField: UITextField = configurePasswordField(placeholderText: "Новый пароль", yPossition: 646)
     
-    private let surnameLabel: UILabel = UILabel()
-    private let surnameLabelValue: UILabel = UILabel()
+    lazy var topImageView: UIImageView = {
+        $0.image = UIImage(named: "TopImage")
+        $0.frame.size = CGSize(width: view.frame.size.width, height: 156)
+        return $0
+    }(UIImageView())
     
-    private let potokNumber: UILabel = UILabel()
-    private let potokNumberValue: UILabel = UILabel()
+    lazy var accountImageView: UIImageView = {
+        $0.image = UIImage(named: "AccountImage")
+        $0.frame = CGRect(x: 30, y: 189, width: 100, height: 100)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 50
+        return $0
+    }(UIImageView())
     
-    private let addDataButton: UIButton = UIButton()
-    private let clearDataButton: UIButton = UIButton()
-
+    lazy var nameAndSurnameLabel: UILabel = {
+        $0.text = "Дмитрий Макеев"
+        $0.font = .systemFont(ofSize: 16, weight: .bold)
+        $0.frame = CGRect(x: 146, y: 208, width: 140, height: 19)
+        return $0
+    }(UILabel())
+    
+    lazy var editingButton: UIButton = {
+        $0.setTitle("Редактировать", for: .normal)
+        $0.backgroundColor = .blue
+        $0.layer.cornerRadius = 10
+        $0.frame = CGRect(x: 146, y: 236, width: 147, height: 34)
+        $0.addAction(editingButtonAction, for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    lazy var addDescriptionLabel: UILabel = {
+        $0.text = "Добавить описание"
+        $0.frame = CGRect(x: 30, y: 341, width: 160, height: 19)
+        return $0
+    }(UILabel())
+    
+    lazy var descriptionTextView: UITextView = {
+        $0.backgroundColor = .systemGray5
+        $0.frame = CGRect(x: 30, y: 369, width: view.frame.size.width - 60, height: 150)
+        $0.layer.cornerRadius = 20
+        return $0
+    }(UITextView())
+    
+    lazy var changePasswordLabel: UILabel = {
+        $0.text = "Изменить пароль"
+        $0.frame = CGRect(x: 30, y: 552, width: 145, height: 19)
+        return $0
+    }(UILabel())
+    
+    lazy var saveButton: UIButton = {
+        $0.setTitle("Coxpaнить", for: .normal)
+        $0.backgroundColor = .blue
+        $0.layer.cornerRadius = 10
+        $0.frame = CGRect(x: 30, y: 760, width: view.frame.size.width - 60, height: 55)
+        $0.addAction(saveButtonAction, for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    lazy var saveButtonAction: UIAction = UIAction{ _ in
+        self.saveButton.backgroundColor = .black
+        self.saveButton.setTitle("Пароль успешно изменен!", for: .normal)
+    }
+    
+    lazy var editingButtonAction: UIAction = UIAction{ _ in
+        self.editingButton.backgroundColor = .black
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupView()
-        setupConstraints()
-        
     }
     
     private func setupView() {
         
         view.backgroundColor = .white
         
-        nameLabel.text = "Имя: "
-        nameLabelValue.text = "Дмитрий"
-        nameLabelValue.textColor = .red
-        
-        surnameLabel.text = "Фамилия: "
-        surnameLabelValue.text = "Макеев"
-        surnameLabelValue.textColor = .red
-        
-        potokNumber.text = "Номер потока: "
-        potokNumberValue.text = "ios 6"
-        potokNumberValue.textColor = .red
-        
-        addDataButton.tintColor = .white
-        addDataButton.setTitle("Добавить данные", for: .normal)
-        addDataButton.backgroundColor = .black
-        addDataButton.layer.cornerRadius = 15
-        
-        clearDataButton.tintColor = .systemBlue
-        clearDataButton.setTitle("Очистить данные", for: .disabled)
-        
-        view.addSubview(nameLabel)
-        view.addSubview(nameLabelValue)
-        view.addSubview(surnameLabel)
-        view.addSubview(surnameLabelValue)
-        view.addSubview(potokNumber)
-        view.addSubview(potokNumberValue)
-        view.addSubview(addDataButton)
-        view.addSubview(clearDataButton)
-        
+        view.addSubview(topImageView)
+        view.addSubview(accountImageView)
+        view.addSubview(nameAndSurnameLabel)
+        view.addSubview(editingButton)
+        view.addSubview(addDescriptionLabel)
+        view.addSubview(descriptionTextView)
+        view.addSubview(changePasswordLabel)
+        view.addSubview(oldPasswordField)
+        view.addSubview(newPasswordField)
+        view.addSubview(saveButton)
     }
     
-    private func setupConstraints() {
-        
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        nameLabelValue.translatesAutoresizingMaskIntoConstraints = false
-        surnameLabel.translatesAutoresizingMaskIntoConstraints = false
-        surnameLabelValue.translatesAutoresizingMaskIntoConstraints = false
-        potokNumber.translatesAutoresizingMaskIntoConstraints = false
-        potokNumberValue.translatesAutoresizingMaskIntoConstraints = false
-        addDataButton.translatesAutoresizingMaskIntoConstraints = false
-        clearDataButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            nameLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 70),
-            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            
-            nameLabelValue.topAnchor.constraint(equalTo: nameLabel.topAnchor),
-            nameLabelValue.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 5),
-            
-            surnameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
-            surnameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
-            
-            surnameLabelValue.topAnchor.constraint(equalTo: surnameLabel.topAnchor),
-            surnameLabelValue.leadingAnchor.constraint(equalTo: surnameLabel.trailingAnchor, constant: 5),
-            
-            potokNumber.topAnchor.constraint(equalTo: surnameLabel.bottomAnchor,constant: 10),
-            potokNumber.leadingAnchor.constraint(equalTo: surnameLabel.leadingAnchor),
-            
-            potokNumberValue.topAnchor.constraint(equalTo: potokNumber.topAnchor),
-            potokNumberValue.leadingAnchor.constraint(equalTo: potokNumber.trailingAnchor, constant: 5),
-            
-            addDataButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 650),
-            addDataButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
-            addDataButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
-            addDataButton.heightAnchor.constraint(equalToConstant: 70),
-            
-            clearDataButton.topAnchor.constraint(equalTo: addDataButton.bottomAnchor, constant: 20),
-            clearDataButton.leadingAnchor.constraint(equalTo: addDataButton.leadingAnchor),
-            clearDataButton.trailingAnchor.constraint(equalTo: addDataButton.trailingAnchor),
-            clearDataButton.heightAnchor.constraint(equalToConstant: 40)
-        ])
+    private func configurePasswordField(placeholderText: String, yPossition: Double) -> UITextField {
+        let passwordField = UITextField()
+        passwordField.backgroundColor = .systemGray5
+        passwordField.frame = CGRect(x: 30, y: yPossition, width: view.frame.size.width - 60.0, height: 52)
+        passwordField.placeholder = placeholderText
+        passwordField.layer.cornerRadius = 10
+        passwordField.isSecureTextEntry = true
+        return passwordField
     }
 }
