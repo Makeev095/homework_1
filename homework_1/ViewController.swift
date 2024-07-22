7114c1dec16315fd3575ed1ab536e9ba9bfeaff7
Dmitriy Makeev <makeev095095@mail.ru>
2024-07-22 14:13:51 +0300

    homework_2.1

--- homework_1/ViewController.swift
+++ homework_1/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var oldPasswordField: UITextField = configurePasswordField(placeholderText: "Старый пароль", yPossition: 580)
-    lazy var newPasswordField: UITextField = configurePasswordField(placeholderText: "Новый пароль", yPossition: 646)
+    lazy var oldPasswordField: UITextField = configurePasswordField(placeholderText: "Старый пароль", yPossition: changePasswordLabel.frame.minY + 28.0)
+    lazy var newPasswordField: UITextField = configurePasswordField(placeholderText: "Новый пароль", yPossition: oldPasswordField.frame.minY + 66.0)
     
     lazy var topImageView: UIImageView = {
         $0.image = UIImage(named: "TopImage")
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     
     lazy var accountImageView: UIImageView = {
         $0.image = UIImage(named: "AccountImage")
-        $0.frame = CGRect(x: 30, y: 189, width: 100, height: 100)
+        $0.frame = CGRect(x: 30, y: topImageView.frame.maxY + 33, width: 100, height: 100)
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 50
         return $0
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
     lazy var nameAndSurnameLabel: UILabel = {
         $0.text = "Дмитрий Макеев"
         $0.font = .systemFont(ofSize: 16, weight: .bold)
-        $0.frame = CGRect(x: 146, y: 208, width: 140, height: 19)
+        $0.frame = CGRect(x: 146, y: topImageView.frame.maxY + 52, width: 140, height: 19)
         return $0
     }(UILabel())
     
@@ -37,27 +37,27 @@ class ViewController: UIViewController {
         $0.setTitle("Редактировать", for: .normal)
         $0.backgroundColor = .blue
         $0.layer.cornerRadius = 10
-        $0.frame = CGRect(x: 146, y: 236, width: 147, height: 34)
+        $0.frame = CGRect(x: 146, y: nameAndSurnameLabel.frame.minY + 28, width: 147, height: 34)
         $0.addAction(editingButtonAction, for: .touchUpInside)
         return $0
     }(UIButton())
     
     lazy var addDescriptionLabel: UILabel = {
         $0.text = "Добавить описание"
-        $0.frame = CGRect(x: 30, y: 341, width: 160, height: 19)
+        $0.frame = CGRect(x: 30, y: accountImageView.frame.minY + 152, width: 160, height: 19)
         return $0
     }(UILabel())
     
     lazy var descriptionTextView: UITextView = {
         $0.backgroundColor = .systemGray5
-        $0.frame = CGRect(x: 30, y: 369, width: view.frame.size.width - 60, height: 150)
+        $0.frame = CGRect(x: 30, y: addDescriptionLabel.frame.minY + 28, width: view.frame.size.width - 60, height: 150)
         $0.layer.cornerRadius = 20
         return $0
     }(UITextView())
     
     lazy var changePasswordLabel: UILabel = {
         $0.text = "Изменить пароль"
-        $0.frame = CGRect(x: 30, y: 552, width: 145, height: 19)
+        $0.frame = CGRect(x: 30, y: descriptionTextView.frame.minY + 183, width: 145, height: 19)
         return $0
     }(UILabel())
     
@@ -65,7 +65,7 @@ class ViewController: UIViewController {
         $0.setTitle("Coxpaнить", for: .normal)
         $0.backgroundColor = .blue
         $0.layer.cornerRadius = 10
-        $0.frame = CGRect(x: 30, y: 760, width: view.frame.size.width - 60, height: 55)
+        $0.frame = CGRect(x: 30, y: newPasswordField.frame.minY + 120, width: view.frame.size.width - 60, height: 55)
         $0.addAction(saveButtonAction, for: .touchUpInside)
         return $0
     }(UIButton())
